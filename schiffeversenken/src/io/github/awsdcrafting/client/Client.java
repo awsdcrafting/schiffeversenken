06f278b93e5f90e8ee0987fb2a382deb879f41e5
eLCi <eLCi@10.0.0.7>
2015-08-04 22:43:19 +0200

    5

--- schiffeversenken/src/io/github/awsdcrafting/client/Client.java
+++ schiffeversenken/src/io/github/awsdcrafting/client/Client.java
@@ -2,13 +2,27 @@ package io.github.awsdcrafting.client;
 
 import io.github.craftqq.utility.Observable;
 import io.github.craftqq.utility.Observer;
+import io.github.craftqq.FileIO.DateiLeser;
+import io.github.craftqq.FileIO.DateiSchreiber;
 
 public class Client implements Observer
 {
+	String ReadedFile;
 	
-	public Client()
-	{
+	public Client()  //das ist der konstruktor
+	{		
+		DateiLeser myLeserling;
+		myLeserling.leseDatei("User.txt",ReadedFile);
 		
+		/*Dateiformat/Zeilenformat: USERID;Name;3stelliges Passwort 
+		1. Zeile Hauptuser auf dem PC
+		alle weiteren Zeilien zusätzliche User die sich hier mal eingeloggt haben aber ohne Passwort
+		*/
+	}
+	
+	public void SaveUserAndPW ()
+	{
+		DateiSchreiber mySchreiberling;
 	}
 	
 	@Override
